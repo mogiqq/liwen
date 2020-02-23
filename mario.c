b266364641cbe375f5b32a4ce32f4aa8ce4d140f
mogiqq <mogiqq@users.noreply.github.com>
2020-02-23 13:10:38 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -8,17 +8,17 @@ int main(void)
     {
         n = get_int("Height: ");
     }
-    while (n<1 || n>8);
+    while (n < 1 || n > 8);
     for (int i = 0; i < n; i++)
     {
         for (int s = n - i; s > 1 ; s--)
         {   
             printf(" ");
         }
-        for (int h = 0; h < i+1; h++)
+        for (int h = 0; h < i + 1; h++)
         {
             printf("#");
         }
-    printf("\n");
+        printf("\n");
     }
 }
