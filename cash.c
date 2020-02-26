742fb47dbc49a76b8396d7ba61401c3060b3fa97
mogiqq <mogiqq@users.noreply.github.com>
2020-02-26 15:19:42 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -7,16 +7,19 @@ int main(void)
     float dollars;
     do
     {
+        // get float number
         dollars = get_float("how much change is owed: ");
     }
+    // check if dollars is non negative
     while (dollars < 0);
+    // round dollars as integer
     int cents = round(dollars * 100);
-
+    // check how many coins require
     int quaters = cents / 25;
-    int dimes = (cents % 25)/10;
-    int nickels = ((cents % 25) % 10)/5;
-    int pennies = (((cents % 25) %10) %5);
+    int dimes = (cents % 25) / 10;
+    int nickels = ((cents % 25) % 10) / 5;
+    int pennies = (((cents % 25) % 10) % 5);
 
-    printf("%d\n", quaters+dimes+nickels+pennies);
+    printf("%d\n", quaters + dimes + nickels + pennies);
     
 }
