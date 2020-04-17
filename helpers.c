1e431c4e8d17f08e36d565eb11dcdd9046322253
mogiqq <mogiqq@users.noreply.github.com>
2020-04-17 15:16:40 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -4,7 +4,7 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int avg;
+    float avg;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
