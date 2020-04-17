c40a14ef14c633c3cc7c4dfcaa170858ec73f8c2
mogiqq <mogiqq@users.noreply.github.com>
2020-04-17 15:14:50 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -10,9 +10,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
-            image[i][j].rgbtBlue = round(avg);
-            image[i][j].rgbtGreen = round(avg);
-            image[i][j].rgbtRed = round(avg);
+            image[i][j].rgbtBlue = roundf(avg);
+            image[i][j].rgbtGreen = roundf(avg);
+            image[i][j].rgbtRed = roundf(avg);
         }
     }
     return;
